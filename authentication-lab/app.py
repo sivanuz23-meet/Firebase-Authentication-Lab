fdb42c46bfdce42b5b34ca4b6b577f9bc0e9f4ab
sivanuz23-meet <109904738+sivanuz23-meet@users.noreply.github.com>
2022-08-01 10:11:44 +0300

    Finished Lab

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -5,16 +5,52 @@ import pyrebase
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
+config = {
+
+  "apiKey": "AIzaSyBflH-j9YSw2K9GnHA5TxvvceVw2lR0l00",
+
+  "authDomain": "database-lab-3fb7c.firebaseapp.com",
+
+  "projectId": "database-lab-3fb7c",
+
+  "storageBucket": "database-lab-3fb7c.appspot.com",
+
+  "messagingSenderId": "862144256115",
+
+  "appId": "1:862144256115:web:77442267a9078423d2b206",
+
+  "measurementId": "G-QHEETK0LS6", "databaseURL": ""
+
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
+
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
-    return render_template("signin.html")
-
+	if request.method == 'POST':
+		try:
+			login_session['email'] = request.form['email']
+			login_session['password'] = request.form['password']
+			return redirect(url_for('add_tweet'))
+		except:
+			print("Authentication Failed")
+			return render_template("signin.html")
+	return render_template("signin.html")
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
-
+	if request.method == 'POST':
+		try:
+			login_session['email'] = request.form['email']
+			login_session['password'] = request.form['password']
+			return redirect(url_for('add_tweet'))
+		except:
+			print("Authentication Failed")
+			return render_template("signup.html")
+	return render_template("signup.html")
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
