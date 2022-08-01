f756f6c621f21adecdf3f39e68083321f47ce8db
sivanuz23-meet <109904738+sivanuz23-meet@users.noreply.github.com>
2022-08-01 12:45:23 +0300

    bonus timestamp still needs work

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
-
+import datetime
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
@@ -27,36 +27,45 @@ firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 db = firebase.database()
 
+
 @app.route('/', methods=['GET', 'POST'])
 def signin():
 	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']
 		try:
-			login_session['email'] = request.form['email']
-			login_session['password'] = request.form['password']
+			login_session['user'] = auth.sign_in_with_email_and_password(email, password)
 			return redirect(url_for('add_tweet'))
 		except:
 			print("Authentication Failed")
-			return render_template("signin.html")
 	return render_template("signin.html")
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
 	if request.method == 'POST':
 		try:
-			login_session['email'] = request.form['email']
-			login_session['password'] = request.form['password']
-			user = {"full_name": request.form['full_name'], "username": request.form['username'], "bio": request.form['bio']}
-			db.child("Users").child(login_session['user']['localId']).get().val()
+			email = request.form['email']
+			password = request.form['password']
+			user = {"email": email, "password": password, "full_name": request.form['full_name'], "username": request.form['username'], "bio": request.form['bio']}
+			login_session['user'] = auth.create_user_with_email_and_password(email, password)
+			db.child("Users").child(login_session['user']['localId']).set(user)
 			return redirect(url_for('add_tweet'))
 		except:
-			print("Authentication Failed")
-			return render_template("signup.html")
+			print("error")
 	return render_template("signup.html")
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
-    return render_template("add_tweet.html")
+	if request.method == "POST":
+		ts = datetime.datetime.now().timestamp()
+		tweet = {"title": request.form['title'], "text": request.form['text'], "uid": login_session['user']['localId'], "timestamp": ts }
+		db.child("Tweets").push(tweet)
+		return redirect(url_for('all_tweets'))
+	return render_template("add_tweet.html")
+@app.route('/all_tweets', methods=['GET', 'POST'])
+def all_tweets():
+	return render_template("all_tweets.html", tweets=db.child("Tweets").get().val())
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
